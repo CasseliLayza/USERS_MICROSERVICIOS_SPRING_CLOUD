85054c00c6c572ef4acc3165d7eecb5811eaea89
CasseliLayza <casseli.layza@gmail.com>
2025-04-25 19:02:42 -0500

    feat: init user microservice spring cloud

---
+++ src/main/java/com/backend/springcloud/mcsv/users/UsersMicroserviciosSpringCloudApplication.java
@@ -0,0 +1,13 @@
+package com.backend.springcloud.mcsv.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersMicroserviciosSpringCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersMicroserviciosSpringCloudApplication.class, args);
+	}
+
+}
